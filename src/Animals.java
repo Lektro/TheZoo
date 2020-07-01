f44d92ea50787f34697a7d1049c46dfc2019ef47
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-07-01 11:37:51 +0200

    made the first classes and added variables

---
+++ src/Animals.java
@@ -0,0 +1,16 @@
+public class Animals extends Zoo{
+
+    /** Class variables */
+    public String name;
+    public int age;
+    public int weight;
+    public char sex;
+    public String color;
+
+    /** default constructor */
+    public Animals(){}
+
+    /** Constructor with all variables */
+    public Animals(String name, Int age, int weight, char sex, String color){}
+
+}
